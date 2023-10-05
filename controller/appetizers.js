8775d3524ade7ee2b613cafb58d6353ef343d6f8
Robert <rcastor0000@gmail.com>
2023-10-05 18:04:02 -0500

    Robert Castor

---
+++ controller/appetizers.js
@@ -0,0 +1,42 @@
+const apps = [
+    { 
+        app: "Cheese Sticks",
+        description: "",
+        price: `$${15}`,
+    },
+    {
+        name: "Fried Pickles",
+        description: "",
+        price: `$${15}`, 
+    },
+    { 
+        name: "Artichoke Dip",
+        description: "",
+        price: `$${25}`,  
+    },
+  ];
+
+
+const getApps = (req, res) => {
+    res.send({apps})
+}
+
+const getApp = (req, res) => {
+    res.send(apps[req.params.id])
+}
+
+const a4m = (req, res) => {
+    let dIndex = Math.floor(Math.random() * apps.length)
+    res.send(apps[dIndex])
+}
+
+module.exports = {
+    getApps,
+    getApp,
+    a4m
+}
+
+
+
+
+
---
+++ controller/entrees.js
@@ -0,0 +1,42 @@
+const entrees = [
+    { 
+        app: "Duck",
+        description: "Duck breast, super greasy and doesnt taste that great, but its still kinda good.",
+        price: `$${40}`,
+    },
+    {
+        name: "Ribeye",
+        description: "Slab of red meat with/without side of rice or seasoned potatoes.",
+        price: `$${55}`, 
+    },
+    { 
+        name: "Salmon",  
+        description: "Slab of Salmon with/without side of rice or seasoned potatoes.",
+        price: `$${28}`,
+    },
+  ];
+
+
+  const getEntrees = (req, res) => {
+    res.send({entrees})
+}
+
+const getEntree = (req, res) => {
+    res.send(entrees[req.params.id])
+}
+
+const e4m = (req, res) => {
+    let dIndex = Math.floor(Math.random() * entrees.length)
+    res.send(entrees[dIndex])
+}
+
+
+module.exports = {
+    getEntrees,
+    getEntree,
+    e4m
+}
+
+
+
+
